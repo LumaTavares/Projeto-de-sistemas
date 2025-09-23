ee90eede82f313030b330b79e4994b1880bab97b
LumaTavares <lumatavares@gmail.com>
2025-09-23 14:18:19 -0300

    bleeh

--- calculadora/calculadora/models/operacoes.py
+++ calculadora/calculadora/models/operacoes.py
@@ -3,7 +3,7 @@ class Operacoes:
         return a+b
     def subtracao(self, a:float,b:float):
         return a-b
-    def divição(self, a:float,b:float):
+    def dividir(self, a:float,b:float):
         return a//b
     def multiplicar(self,a:float,b:float):
         return a*b 
